b0dcd07efa87df432b088847f24497ef4b4522f6
Kalleb <kallebjs07@gmail.com>
2025-01-04 23:47:05 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-scrip
+// Navegação suave
+const navLinks = document.querySelectorAll('header nav ul li a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 60,
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Alterar cabeçalho ao rolar
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
